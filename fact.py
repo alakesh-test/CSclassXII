f9de720af922ace03283bf75ba408a0b26a11f93
Alakx <icvc.a10@gmail.com>
2020-06-20 11:26:47 +0530

    Factorial

---
+++ fact.py
@@ -0,0 +1,14 @@
+#q: Write a function to find the factorial of a natural number.
+# 5 --> 5x4x3x2x1 or 5 --> 1x2X3X4X5
+
+def Fact(n):
+    f=1
+    for i in range(1,n+1):        # range(5) --> 0 1 2 3 4
+           f=f*i
+    return f
+
+#__main
+
+x= int(input("Enter a value to calculte factorial :"))
+val=Fact(x)
+print("The factorial of ",x, "is : ",val)
